4457fc9bbe1f3c75627fd9a9615dc568d0f958bb
Daniel Gilchrist <danniieelg@gmail.com>
2023-03-20 20:39:14 +0000

    Refactor: Avoid nesting

--- src/main.rs
+++ src/main.rs
@@ -5,37 +5,32 @@ use crate::command_matcher::CommandMatcher;
 use crate::history::History;
 
 fn main() {
-    match History::new().get() {
-        Ok(history) => {
-            let dummy_command = "cargo bun";
-            let matcher = CommandMatcher::new(history);
-
-            match matcher.find_match(dummy_command) {
-                Some(mut suggestions) => {
-                    suggestions.sort_by(|suggestion1, suggestion2| {
-                        suggestion1
-                            .similarity
-                            .partial_cmp(&suggestion2.similarity)
-                            .unwrap()
-                    });
-                    suggestions.reverse();
-
-                    let suggested_commands = suggestions
-                        .into_iter()
-                        .map(|suggestion| suggestion.command)
-                        .collect::<Vec<String>>();
-
-                    println!("Suggesions:\n{:?}", suggested_commands);
-                }
-
-                None => {
-                    println!("No suggestions dB^(");
-                }
-            }
-        }
-
+    let history = match History::new().get() {
+        Ok(history) => history,
         Err(error) => {
             eprintln!("{:?}", error);
+            return;
         }
-    }
+    };
+
+    let dummy_command = "cargo bun";
+    let Some(mut suggestions) = CommandMatcher::new(history).find_match(dummy_command) else {
+        eprintln!("No suggestions dB^(");
+        return;
+    };
+
+    suggestions.sort_by(|suggestion1, suggestion2| {
+        suggestion1
+            .similarity
+            .partial_cmp(&suggestion2.similarity)
+            .unwrap()
+    });
+    suggestions.reverse();
+
+    let suggested_commands = suggestions
+        .into_iter()
+        .map(|suggestion| suggestion.command)
+        .collect::<Vec<String>>();
+
+    println!("Suggesions:\n{:?}", suggested_commands);
 }
