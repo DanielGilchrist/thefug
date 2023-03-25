5d0eae2b5293a6671b936feb56567bf44adbc106
Daniel Gilchrist <danniieelg@gmail.com>
2023-03-25 15:25:12 +0000

    Replace promkit with inquire crate (#8)

--- src/selector.rs
+++ src/selector.rs
@@ -1,37 +1,20 @@
-use promkit::{
-    build::Builder,
-    crossterm::style,
-    register::Register,
-    select::{self, State},
-    selectbox::SelectBox,
-    Prompt,
-};
-use std::io::Error;
+use inquire::{error::InquireResult, Select};
 
 pub struct Selector {
     command: String,
     commands: Vec<String>,
 }
 
+// TODO: Don't rely on a crate for this
 impl Selector {
     pub fn new(command: String, commands: Vec<String>) -> Self {
         Self { command, commands }
     }
 
-    pub fn show(&self) -> Result<String, Error> {
-        let mut selector = self.build_selector()?;
-        selector.run()
-    }
-
-    fn build_selector(&self) -> promkit::Result<Prompt<State>> {
-        let mut selectbox = Box::<SelectBox>::default();
-        selectbox.register_all(&self.commands);
-
+    pub fn show(&self) -> InquireResult<String> {
         let command = &self.command;
-        select::Builder::default()
-            .title(format!("The fug? ({command})"))
-            .title_color(style::Color::DarkGreen)
-            .selectbox(selectbox)
-            .build()
+        let commands = self.commands.clone();
+
+        Select::new(&format!("The fug? ({command})"), commands).prompt()
     }
 }
